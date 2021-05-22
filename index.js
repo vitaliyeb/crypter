e02585ab1b04b701f9dade10e78b5f6bc19e14f8
vitaliyeb <vlvlv.vovov@bk.ru>
2021-05-22 15:01:50 +0500

    доделал логику

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
+
+
+
 
 (async () => {
-    const searchBTC = async (page) => {
-        const totalBTC = await page.$$eval('.js-balances-bitcoin [data-original-title="Total balance"]', (el) => el[0].textContent.trim());
+    const dataLogger = (url, totalBTC) => {
+        const filePath = path.join(__dirname, 'log.txt');
 
-
-        if (totalBTC === "0") {
-            await page.$$eval('.pagination .page-link', (links) => {links.find(el => el.textContent.trim() === "Random").click()});
-        }
+        fs.appendFile(filePath, `${url} ---> ${totalBTC} \n`, (err) => {
+            if (err) throw err;
+            console.log('The "data to append" was appended to file!');
+        })
     }
 
-    const browser = await puppeteer.launch({ headless: false });
+    const searchBTC = async (page) => {
+        let totalBTC;
+        let intervalId;
 
+        await new Promise((res) => intervalId = setInterval(async () => {
+            totalBTC = await page.$$eval('.js-balances-bitcoin [data-original-title="Total balance"]', (el) => el[0]?.textContent?.trim());
+            if (totalBTC !== 'undefined') res(clearInterval(intervalId));
+        }, 100))
+
+        if (totalBTC !== "0") dataLogger(page.url(), totalBTC);
+
+        await page.$$eval('.pagination .page-link', (links) => {
+            links.find(el => el.textContent.trim() === "Random").click();
+            setTimeout(() => {
+                if(window.location.href.includes("#google_vignette")) window.location.reload();
+            }, 2000);
+        });
+    }
+
+    const browser = await puppeteer.launch({ headless: false, defaultViewport: { width: 1200, height: 1000} });
     const page = await browser.newPage();
 
     page.on('load', () => searchBTC(page));
+
     await page.goto(`https://privatekeys.pw/keys/bitcoin/${Math.round(Math.random() * 99999999e10)}`)
 
 
