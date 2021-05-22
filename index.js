002340b6abb4390046af03737c8ea4202994805c
vitaliyeb <vlvlv.vovov@bk.ru>
2021-05-22 13:23:14 +0500

    рандомная страничка если на текущей старничке 0 битков

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const puppeteer = require('puppeteer');
 (async () => {
     const searchBTC = async (page) => {
         const totalBTC = await page.$$eval('.js-balances-bitcoin [data-original-title="Total balance"]', (el) => el[0].textContent.trim());
-        console.log(divCount)
+
+
+        if (totalBTC === "0") {
+            await page.$$eval('.pagination .page-link', (links) => {links.find(el => el.textContent.trim() === "Random").click()});
+        }
     }
 
     const browser = await puppeteer.launch({ headless: false });
